4f761979564d70bc308ed748ce4b15ae77a62a42
Nikhil Yewale <yewale.nikhil65@gmail.com>
2021-12-08 02:39:34 +0530

    hermite based finite-difference weights

--- src/Differentiation/fornberg.jl
+++ src/Differentiation/fornberg.jl
@@ -39,5 +39,39 @@ function fornberg(m::Int, z::S, x::AbstractVector; nd = length(x)) where S <: Re
         c1 = c2;
     end
     c = C[:,end]; 	# last column of c gives desired row vector
-#    c[(n÷2) + 1] -= sum(c)
+    c[(n÷2) + 1] -= sum(c)
+    return c
+end
+
+"""
+Based on MATLAB routine in paper , An algorithm for calculating Hermite-based finite difference weights by Bengt Fornberg.
+Paper found at https://www.colorado.edu/amath/sites/default/files/attached-files/2020_f_hermite-fd_ima_j_num_anal.pdf
+"""
+function fornberg_hermite(m::Int, z::S, x::AbstractVector) where S <: Real
+    nd = length(x);
+    c = zeros(m+2, nd);
+    c[2,1] = 1;
+    A = x .- x';
+    s = sum(1 ./ (A + I(nd)), dims=1) .- 1;
+    b = cumprod([ones(nd) A], dims = 2);
+    rmat = repeat(0:m+1, 1,nd-1);
+    d0 = diag(b);
+    d1 = d0[1:nd-1]./d0[2:nd];
+    for i ∈ 2:nd
+        mn = min(i, m+1);
+        c[2:mn+1,i] .= d1[i-1]*(rmat[1:mn, 1].*c[1:mn,i-1] - (x[i-1]-z)*c[2:mn+1,i-1]);
+        c[2:mn+1,1:i-1] .= ((x[i]-z)*c[2:mn+1,1:i-1] - rmat[1:mn,1:i-1].*c[1:mn,1:i-1])./transpose(x[i] .- x[1:i-1]);
+    end
+    c2 = zeros(m+2, nd)
+    cp = cumprod([1; 1:m])
+    c = c[setdiff(1:end, 1), :]
+    cc = c./cp
+    for k=1:m+1
+        c2[k+1,:] = sum(cc[1:k,:].*cc[k:-1:1,:], dims = 1);
+    end
+    e = c2[1:m+1,:] - transpose(x .- z).*c2[2:m+2,:];
+    d = c2[2:m+2,:] + 2*s.*e;
+    d = d.*cp;
+    e = e.*cp;
+    return c[end,:], d[end,:] ,e[end,:]
 end
