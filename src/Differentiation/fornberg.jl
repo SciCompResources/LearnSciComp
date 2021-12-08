aa128295de0cddc7728a9491466cd32ea0d93874
Nikhil Yewale <yewale.nikhil65@gmail.com>
2021-12-09 01:44:39 +0530

    adding a kwarg `dfdx` for hermite-based finite differences weights
    dfdx == false is default for classical fornberg algorithm

--- src/Differentiation/fornberg.jl
+++ src/Differentiation/fornberg.jl
@@ -1,15 +1,18 @@
 """ Given:
     z : location at which we want to approximate the derivative (may
-    but need not be a grid point),
+        but need not be a grid point),
     x = {x₀ , x₁ , x₂, .... , xₙ} : grid point locations which the stencil is to extend over (distinct,
-    otherwise arbitrary),
+        otherwise arbitrary),
     n = N - 1  : one less than the number of grid points, and
     m  : highest derivative for which weights are sought,
+    dfdx :  optional argument to consider weights for first derivative or not
+            (dfdx == true , calculates weights using hermite-based algorithm)
 
 Output Parameter
-    c[0:nd,0:m] weights at grid locations x[0:n] for derivatives of order 0:m, found in c[0:n,0:m]
+    c : weights to approximate derivative using function values only (dfdx == false)
+    d ,e : weights to approximate derivative using function and its first derivative values (dfdx == true)
 """
-@views function fornberg(m::Int, z::S, x::AbstractVector; nd = length(x)) where S <: Real
+function fornberg(m::Int, z::S, x::AbstractVector; nd = length(x); dfdx::Bool = false) where S <: Real
     nd >= m || error("number of data-points must be larger than order of derivative")
     n = nd - 1;    #
     c1 = one(S);
@@ -39,39 +42,24 @@ Output Parameter
         c1 = c2;
     end
     c = C[:,end]; 	# last column of c gives desired row vector
-    c[(n÷2) + 1] -= sum(c)
-    return c
-end
+    
+    if dfdx == false
+        return c
+    else
+        A = x .- x';
+        s = sum(1 ./ (A + I(nd)), dims = 1) .- 1;
+        cp = factorial.(0:m);
+        cc = C./cp'
+        c̃ = zeros(nd, m+2);
+        for k in 1:m+1
+           c̃[:,k+1] = sum(cc[:,1:k].*cc[:,k:-1:1], dims = 2);
+        end
+        E = c̃[:,1:m+1] - (x .- z).*c̃[:,2:m+2];
+        D = c̃[:,2:m+2] + 2*E.*s';
+        D = D.*cp';
+        E = E.*cp';
 
-"""
-Based on MATLAB routine in paper , An algorithm for calculating Hermite-based finite difference weights by Bengt Fornberg.
-Paper found at https://www.colorado.edu/amath/sites/default/files/attached-files/2020_f_hermite-fd_ima_j_num_anal.pdf
-"""
-@views function fornberg_hermite(m::Int, z::S, x::AbstractVector) where S <: Real
-    nd = length(x);
-    c = zeros(m+2, nd);
-    c[2,1] = 1;
-    A = x .- x';
-    s = sum(1 ./ (A + I(nd)), dims=1) .- 1;
-    b = cumprod([ones(nd) A], dims = 2);
-    rmat = repeat(0:m+1, 1,nd-1);
-    d0 = diag(b);
-    d1 = d0[1:nd-1]./d0[2:nd];
-    for i ∈ 2:nd
-        mn = min(i, m+1);
-        c[2:mn+1,i] .= d1[i-1]*(rmat[1:mn, 1].*c[1:mn,i-1] - (x[i-1]-z)*c[2:mn+1,i-1]);
-        c[2:mn+1,1:i-1] .= ((x[i]-z)*c[2:mn+1,1:i-1] - rmat[1:mn,1:i-1].*c[1:mn,1:i-1])./transpose(x[i] .- x[1:i-1]);
+        d = D[:,end];   e = E[:,end]
+        return d, e
     end
-    c2 = zeros(m+2, nd)
-    cp = factorial.(0:m)
-    c = c[setdiff(1:end, 1), :]
-    cc = c./cp
-    for k=1:m+1
-        c2[k+1,:] = sum(cc[1:k,:].*cc[k:-1:1,:], dims = 1);
-    end
-    e = c2[1:m+1,:] - transpose(x .- z).*c2[2:m+2,:];
-    d = c2[2:m+2,:] + 2*s.*e;
-    d = d.*cp;
-    e = e.*cp;
-    return c[end,:], d[end,:] ,e[end,:]
 end
