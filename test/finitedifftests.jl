24287394c388b7b8dc3a4ad0f83cc0fe79c79fd5
Nikhil Yewale <yewale.nikhil65@gmail.com>
2021-12-09 02:16:41 +0530

    test for fornberg-hermite

--- test/finitedifftests.jl
+++ test/finitedifftests.jl
@@ -1,7 +1,7 @@
 @info "Starting finite difference tests"
 using LearnSciComp
 
-@testset "weights from fornberg(1998)" begin
+@testset "weights from fornberg(1998) & fornberg-hermite(2020)" begin
     order = 2; z = 0; x = [-1, 0, 1.0];
     @test fornberg(order, z, x) == [1,-2,1]  # central difference of second-derivative with unit-step
 
@@ -20,4 +20,9 @@ using LearnSciComp
     # forward-diff of third derivative with order of accuracy == 3
     order = 3; z = 0; x = [0,1,2,3,4,5]
     @test fornberg(order, z, x) == [-17/4,	71/4	,−59/2,	49/2,	−41/4,	7/4]
+    
+    order = 3; z = 0; x = collect(-3:3)
+    d, e = fornberg(order, z, x;dfdx = true)
+    @test d ≈ [-167/18000, -963/2000, -171/16,0,171/16,963/2000,167/18000]
+    @test e ≈ [-1/600,-27/200,-27/8,-49/3,-27/8,-27/200,-1/600]
 end
